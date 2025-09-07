61909e0a8a2a0b387784f515f36eec884859eeee
Akintayopope <akinpope25@gmail.com>
2025-09-07 17:37:51 -0500

    feat: load card, product, and product overview templates

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require('fs')
 const http = require('http')
 
+
+const cards = fs.readFileSync(`${__dirname}/templates/card_product.html`, 'utf-8')
+
+const products = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
+
+const productsOverview = fs.readFileSync(`${__dirname}/templates/product_overview.html`, 'utf-8')
+
+console.log(productsOverview)
+
 const server = http.createServer((req, res) =>{
     res.end('Server Up and running')
 })
@@ -8,3 +17,4 @@ const server = http.createServer((req, res) =>{
 server.listen(7000, '127.0.0.1', () => {
     console.log('Started Listening')
 })
+
